8bb0d342435d4315fc85ad876cb14ebc6f3f8948
Shannon Grantski <grantski@pm.me>
2020-12-10 16:23:49 -0800

    cut array into string, print to console.
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>

--- Allergies/Program/Program.cs
+++ Allergies/Program/Program.cs
@@ -6,54 +6,55 @@ class Program
   {
     Console.WriteLine("What is your allergy score?");
     int score = int.Parse(Console.ReadLine());
+    string[] arrayName = {"", "", "", "", "", "", "", ""};
 
     if (score >= 128)
     {
       score -= 128;
-      Console.WriteLine("128!");
+      arrayName[7] = "cats";
     } 
     if (score >= 64)
     {
       score -= 64;
-      Console.WriteLine("64!");
+      arrayName[6] = "pollen";
     }
     if (score >= 32)
     {
       score -= 32; 
-      Console.WriteLine("32!!!!");
+      arrayName[5] = "chocolate";
     }
     if (score >= 16)
     {
       score -= 16;
-      Console.WriteLine("it's 16!");
+      arrayName[4] = "tomatoes";
     }
     if (score >= 8)
     {
       score -= 8;
-      Console.WriteLine("!8!");
+      arrayName[3] = "strawberries";
     }
     if (score >= 4)
     {
       score -= 4;
-      Console.WriteLine("4--4--4--4!");
+      arrayName[2] = "shellfish";
     }
     if (score >= 2)
     {
       score -= 2;
-      Console.WriteLine("2...");
+      arrayName[1] = "peanuts";
     }
     if (score >= 1)
     {
       score -= 1;
-      Console.WriteLine("1.");
+      arrayName[0] = "eggs";
+    }       
+    Console.WriteLine("You are allergic to: ");
+    foreach (string allergy in arrayName)
+    {
+      if (allergy != "") 
+      {
+        Console.WriteLine(allergy);
+      }
     }
-    Console.WriteLine("0?");
-    Console.WriteLine(score);
-    // console write "hey u got allergy score right. wanna know what it means. put ur number below :) "
-    // int (variable name) = console read line
-    // if (variable name) > 128, 
-    // cat, variable name - 128, else move on
-    //  etc
-    // etc
   }
 }
